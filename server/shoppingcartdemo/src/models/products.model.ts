9a80cfb8058106cc73153e7bc8d1e1cf4b10c05d
Bellas, Reginald <Reginald.Bellas@usfoods.com>
2020-05-24 20:15:48 -0500

    add products and migrations to mysql and create crud, repo, modal

---
+++ server/shoppingcartdemo/src/models/products.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Products extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  img: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  stocks: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  drugcode: string;
+
+
+  constructor(data?: Partial<Products>) {
+    super(data);
+  }
+}
+
+export interface ProductsRelations {
+  // describe navigational properties here
+}
+
+export type ProductsWithRelations = Products & ProductsRelations;
