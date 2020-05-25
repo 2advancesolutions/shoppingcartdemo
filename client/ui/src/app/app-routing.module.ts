a82d07833b442be00075773fb1ad40891e2b4299
Bellas, Reginald <Reginald.Bellas@usfoods.com>
2020-05-24 22:37:12 -0500

    miss checkin file

--- client/ui/src/app/app-routing.module.ts
+++ client/ui/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-
+  { path: 'history', component: OrderHistoryComponent },
+  { path: 'profile', component: UserProfileComponent },
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
